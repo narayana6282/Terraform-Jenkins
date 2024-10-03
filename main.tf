b3c21f342b0e04c9215be0a9fd24b51279988c8f
Bajiramu1 <balajiarudala@gmail.com>
2024-10-03 15:28:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-1"  
+  region = "ap-south-1"  
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-0ebfd941bbafe70c6" # us-west-2
+  ami           = "ami-037774efca2da0726" # us-west-2
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
